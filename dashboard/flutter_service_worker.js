de2c7a29fd39ec04162aac1768645f321dd70c39
prakhar <prakhar@bu.edu>
2023-12-14 11:56:12 -0500

    added update

--- dashboard/flutter_service_worker.js
+++ dashboard/flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "f066e1a6d0c7dba96fc88e8be58581e4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8a018dcae18101fab0b67087bb7c59fa",
-"/": "8a018dcae18101fab0b67087bb7c59fa",
-"main.dart.js": "0e75a87ec49d76e82252f300b85fb468",
+"index.html": "a06b95bdcc649d44420e729e41d6a39b",
+"/": "a06b95bdcc649d44420e729e41d6a39b",
+"main.dart.js": "aca9b9d51e84d25af577e4d335b64336",
 "manifest.json": "965aede03a78d92b18e4707c4322ca6e",
 "version.json": "43fd00adb27f861579618b7c28d206cb"};
 // The application shell files that are downloaded before a service worker can
