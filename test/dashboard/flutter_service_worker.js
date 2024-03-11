e0dc8e6be6ff03f955d4ab7129fbde5575fccb78
prakhar <prakhar@bu.edu>
2024-03-11 16:10:18 -0400

    added code

--- test/dashboard/flutter_service_worker.js
+++ test/dashboard/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f066e1a6d0c7dba96fc88e8be58581e4
 "assets/assets/icons/rice_2.png": "798bc8b07e13c066e237d20561473de3",
 "assets/assets/icons/rice_3.png": "683220c59e7acc92058e0ead188887db",
 "assets/FontManifest.json": "353c0d54ebf6a3df2e988b0f49bdb2cb",
-"assets/fonts/MaterialIcons-Regular.otf": "2e0e8731a445ec7d604e45d03d537947",
+"assets/fonts/MaterialIcons-Regular.otf": "e3bbad8fb43523b1ee85f8cee7d33a13",
 "assets/NOTICES": "ecde74e1c008a2ac117dcf2defab003c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/packages/eva_icons_flutter/lib/fonts/Eva-Icons.ttf": "b3cfd6832181cbaa3c98988c49d34641",
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "f066e1a6d0c7dba96fc88e8be58581e4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "82bdf18ec932771452e4958d1bf18d43",
-"/": "82bdf18ec932771452e4958d1bf18d43",
-"main.dart.js": "a120a6fec0495f6fb67fd9a640c471fd",
+"index.html": "33419cccea8a73861bb8ad9bf75e0c50",
+"/": "33419cccea8a73861bb8ad9bf75e0c50",
+"main.dart.js": "f28334102f5c3d7d5b078995483fa936",
 "manifest.json": "965aede03a78d92b18e4707c4322ca6e",
 "version.json": "43fd00adb27f861579618b7c28d206cb"};
 // The application shell files that are downloaded before a service worker can
